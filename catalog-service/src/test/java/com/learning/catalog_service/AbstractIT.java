69237e6161714a6bef6af789492c07792dae2926
gnidaan <nid238016566>
2025-10-23 04:44:06 +0530

    spotless

--- catalog-service/src/test/java/com/learning/catalog_service/AbstractIT.java
+++ catalog-service/src/test/java/com/learning/catalog_service/AbstractIT.java
@@ -8,10 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT, properties ={
-        // Optional config server for safety
-        "spring.config.import=optional:configserver:http://localhost:8888",
-        "spring.cloud.config.fail-fast=false"})
+@SpringBootTest(
+        webEnvironment = RANDOM_PORT,
+        properties = {
+            // Optional config server for safety
+            "spring.config.import=optional:configserver:http://localhost:8888",
+            "spring.cloud.config.fail-fast=false"
+        })
 @Import(TestcontainersConfiguration.class)
 public abstract class AbstractIT {
 
