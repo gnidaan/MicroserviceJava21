3b08ae23cd8795cfc1a6d936e1c71db738f564da
gnidaan <nid238016566>
2025-10-23 05:04:05 +0530

    complete off for test

--- catalog-service/src/test/java/com/learning/catalog_service/AbstractIT.java
+++ catalog-service/src/test/java/com/learning/catalog_service/AbstractIT.java
@@ -7,15 +7,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(
         webEnvironment = RANDOM_PORT,
         properties = {
             // Optional config server for safety
+            "spring.cloud.config.enabled=false",
             "spring.config.import=optional:configserver:http://localhost:8888",
             "spring.cloud.config.fail-fast=false"
         })
 @Import(TestcontainersConfiguration.class)
+@ActiveProfiles("test")
 public abstract class AbstractIT {
 
     @LocalServerPort
